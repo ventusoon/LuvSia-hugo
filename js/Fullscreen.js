599ca002763ae63484074cf67d1c54090256f1b3
ventusoon <ventusoon@users.noreply.github.com>
2022-01-26 11:09:25 +0000

    deploy: ventusoon/LuvSia.com@376726bd2778877116f3ce2cb48849c48e442473

--- js/Fullscreen.js
+++ js/Fullscreen.js
@@ -1,57 +1,15 @@
-function goFullscreen(id) {undefined
-
-  var element = document.getElementById(id);
-  
-  var isInFullScreen = (document.fullscreenElement && document.fullscreenElement !== null) ||
-  
-  (document.webkitFullscreenElement && document.webkitFullscreenElement !== null) ||
-  
-  (document.mozFullScreenElement && document.mozFullScreenElement !== null) ||
-  
-  (document.msFullscreenElement && document.msFullscreenElement !== null);
-  
-  var docElm = document.documentElement;
-  
-  if (!isInFullScreen) {undefined
-  
-  if (element.requestFullscreen) {undefined
-  
-  element.requestFullscreen();
-  
-  } else if (element.mozRequestFullScreen) {undefined
-  
-  element.mozRequestFullScreen();
-  
-  } else if (element.webkitRequestFullScreen) {undefined
-  
-  element.webkitRequestFullScreen();
-  
-  } else if (element.msRequestFullscreen) {undefined
-  
-  element.msRequestFullscreen();
-  
+window.onload = function() {
+  scroll()
+}
+function scroll() {
+  if (
+    document.documentElement.scrollHeight <=
+    document.documentElement.clientHeight
+  ) {
+    bodyTag = document.getElementsByTagName("body")[0]
+    bodyTag.style.height =
+      (document.documentElement.clientWidth / screen.width) *
+        screen.height +
+      "px"
   }
-  
-  } else {undefined
-  
-  if (document.exitFullscreen) {undefined
-  
-  document.exitFullscreen();
-  
-  } else if (document.webkitExitFullscreen) {undefined
-  
-  document.webkitExitFullscreen();
-  
-  } else if (document.mozCancelFullScreen) {undefined
-  
-  document.mozCancelFullScreen();
-  
-  } else if (document.msExitFullscreen) {undefined
-  
-  document.msExitFullscreen();
-  
-  }
-  
-  }
-  
-  }
+}
